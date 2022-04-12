329de73533150faf699a86529a35da41575f45d3
lichenchen <lichenchen15@huawei.com>
2022-04-12 16:40:49 +0800

    osAccount_fuzz测试规范整改
    Signed-off-by: lichenchen <lichenchen15@huawei.com>

---
+++ test/fuzztest/osaccount/setosaccountprofilephoto_fuzzer/setosaccountprofilephoto_fuzzer.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "setosaccountprofilephoto_fuzzer.h"
+
+#include <string>
+#include <vector>
+#include "os_account_manager.h"
+#include "account_log_wrapper.h"
+#undef private
+#include "os_account_constants.h"
+
+using namespace std;
+using namespace OHOS::AccountSA;
+const int LOCAL_ID = 100;
+
+namespace OHOS {
+    bool SetOsAccountProfilePhotoFuzzTest(const uint8_t* data, size_t size)
+    {
+        ACCOUNT_LOGI("SetOsAccountProfilePhotoFuzzTest Enter");
+        bool result = false;
+        if (size > 0) {
+            result = OsAccountManager::SetOsAccountProfilePhoto(LOCAL_ID, reinterpret_cast<const char*>(data));
+        }
+        return result;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::SetOsAccountProfilePhotoFuzzTest(data, size);
+    return 0;
+}
+
