96cf4e2e3cf796cc289eaac1556b343c18828d15
lichenchen <lichenchen15@huawei.com>
2022-04-12 11:15:31 +0800

    osAccount_fuzz测试规范整改
    Signed-off-by: lichenchen <lichenchen15@huawei.com>

---
+++ test/fuzztest/osaccount/CreateOsAccount_fuzzer/CreateOsAccount_fuzzer.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "CreateOsAccount_fuzzer.h"
+
+#include <string>
+#include <vector>
+#include "os_account_manager.h"
+#include "account_log_wrapper.h"
+#undef private
+#include "os_account_constants.h"
+
+using namespace std;
+using namespace OHOS::AccountSA;
+const int CONSTANTS_NUMBER_FIVE = 5;
+
+namespace OHOS {
+    bool CreateOsAccountFuzzTest(const uint8_t* data, size_t size)
+    {
+        ACCOUNT_LOGI("CreateOsAccountFuzzTest Enter");
+        bool result = false;
+        if (size > 0) {
+            OsAccountInfo osAccountInfoOne;
+            OsAccountType testType = static_cast<OsAccountType>(size % CONSTANTS_NUMBER_FIVE);
+            result = OsAccountManager::CreateOsAccount(reinterpret_cast<const char*>(data), testType, osAccountInfoOne);
+            if (result == ERR_OK) {
+                ACCOUNT_LOGI("CreateOsAccountFuzzTest RemoveOsAccount");
+                OsAccountManager::RemoveOsAccount(osAccountInfoOne.GetLocalId());
+            }
+        }
+        return result;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::CreateOsAccountFuzzTest(data, size);
+    return 0;
+}
+
---
+++ test/fuzztest/osaccount/SetOsAccountConstraints_fuzzer/SetOsAccountConstraints_fuzzer.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "SetOsAccountConstraints_fuzzer.h"
+
+#include <string>
+#include <vector>
+#include "os_account_manager.h"
+#include "account_log_wrapper.h"
+#undef private
+#include "os_account_constants.h"
+
+using namespace std;
+using namespace OHOS::AccountSA;
+const int LOCAL_ID = 100;
+const int CONSTANTS_NUMBER_TWO = 2;
+const int CONSTANTS_NUMBER_THREE = 3;
+
+namespace OHOS {
+    bool SetOsAccountConstraintsFuzzTest(const uint8_t* data, size_t size)
+    {
+        ACCOUNT_LOGI("SetOsAccountConstraintsFuzzTest Enter");
+        bool result = false;
+        if (size > 0) {
+            std::vector<std::string> CONSTANTS_VECTOR {
+                "constraint.print",
+                "constraint.screen.timeout.set",
+                "constraint.share.into.profile"
+            };
+            int temp = size % CONSTANTS_NUMBER_THREE;
+            string str(reinterpret_cast<const char*>(data));
+            if (!temp) {
+                CONSTANTS_VECTOR.push_back(str);
+            }
+            bool enable = ((size % CONSTANTS_NUMBER_TWO) == 0);
+            result = OsAccountManager::SetOsAccountConstraints(LOCAL_ID, CONSTANTS_VECTOR, enable);
+        }
+        return result;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::SetOsAccountConstraintsFuzzTest(data, size);
+    return 0;
+}
