f3da2bba9fb824d8383cae85b20c1bba5ac1b400
baoyang <baoyang9@huawei.com>
2022-08-15 10:14:42 +0800

    账号模块js返回结果整改
    Signed-off-by: baoyang <baoyang9@huawei.com>

--- interfaces/kits/napi/appaccount/src/napi_app_account_common.cpp
+++ interfaces/kits/napi/appaccount/src/napi_app_account_common.cpp
@@ -837,7 +837,7 @@ void ParseContextWithStrCBArray(napi_env env, napi_callback_info cbInfo, GetAcco
 void ProcessCallbackOrPromise(napi_env env, const CommonAsyncContext *asyncContext, napi_value err, napi_value data)
 {
     ACCOUNT_LOGI("enter");
-    napi_value args[RESULT_COUNT] = {nullptr};
+    napi_value args[RESULT_COUNT] = {NapiGetNull(env), NapiGetNull(env)};
     if (asyncContext->status == napi_ok) {
         args[1] = data;
     } else {
@@ -866,7 +866,7 @@ void ProcessCallbackOrPromiseCBArray(
     napi_env env, const GetAccountsAsyncContext *asyncContext, napi_value err, napi_value data)
 {
     ACCOUNT_LOGI("enter");
-    napi_value args[RESULT_COUNT] = {nullptr};
+    napi_value args[RESULT_COUNT] = {NapiGetNull(env), NapiGetNull(env)};
     if (asyncContext->status == napi_ok) {
         args[1] = data;
     } else {
