0bed57f2c58586db725f51a6af692a511848924b
lichenchen <lichenchen15@huawei.com>
2022-08-24 15:59:16 +0800

    账号主干分支codex问题处理
    Signed-off-by: lichenchen <lichenchen15@huawei.com>

--- interfaces/kits/napi/account_iam/include/napi_account_iam_common.h
+++ interfaces/kits/napi/account_iam/include/napi_account_iam_common.h
@@ -48,8 +48,8 @@ struct IAMAsyncContext {
 };
 
 struct JsIAMCallback {
-    napi_ref onResult;
-    napi_ref onAcquireInfo;
+    napi_ref onResult = nullptr;
+    napi_ref onAcquireInfo = nullptr;
 };
 
 #ifdef HAS_USER_AUTH_PART
@@ -58,7 +58,7 @@ struct IDMCallbackParam {
     JsIAMCallback callback;
     int32_t result;
     int32_t module;
-    int32_t acquire;
+    uint32_t acquire;
     uint64_t credentialId;
 };
 
@@ -85,7 +85,7 @@ struct AuthContext {
 struct IDMContext : public IAMAsyncContext {
     explicit IDMContext(napi_env napiEnv) : IAMAsyncContext(napiEnv) {};
     std::vector<uint8_t> challenge;
-    uint64_t credentialId;
+    uint64_t credentialId = 0;
     std::vector<uint8_t> token;
     AccountSA::CredentialParameters addCredInfo;
     JsIAMCallback callback;
@@ -100,7 +100,7 @@ struct GetAuthInfoContext : public IAMAsyncContext {
 struct GetPropertyContext : public IAMAsyncContext {
     explicit GetPropertyContext(napi_env napiEnv) : IAMAsyncContext(napiEnv) {};
     AccountSA::GetPropertyRequest request;
-    int32_t result;
+    int32_t result = 0;
     int32_t authSubType = 0;
     int32_t remainTimes = 0;
     int32_t freezingTime = 0;
@@ -109,7 +109,7 @@ struct GetPropertyContext : public IAMAsyncContext {
 struct SetPropertyContext : public IAMAsyncContext {
     explicit SetPropertyContext(napi_env napiEnv) : IAMAsyncContext(napiEnv) {};
     AccountSA::SetPropertyRequest request;
-    int32_t result;
+    int32_t result = 0;
 };
 
 class NapiIDMCallback : public AccountSA::IDMCallback {
