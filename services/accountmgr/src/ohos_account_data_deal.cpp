c536fa50267457083a3480160049fbbd2563f143
jidong <jidong4@huawei.com>
2022-10-27 09:19:55 +0800

    fix reviewbot
    Signed-off-by: jidong <jidong4@huawei.com>
    Change-Id: Icc812ede811826acd31f2ce9d80cc0939bcaf402

--- services/accountmgr/src/ohos_account_data_deal.cpp
+++ services/accountmgr/src/ohos_account_data_deal.cpp
@@ -45,7 +45,7 @@ OhosAccountDataDeal::OhosAccountDataDeal(const std::string &configFileDir) : con
     initOk_ = false;
 }
 
-ErrCode OhosAccountDataDeal::Init(std::int32_t userId)
+ErrCode OhosAccountDataDeal::Init(int32_t userId)
 {
     std::string configFile = configFileDir_ + std::to_string(userId) + ACCOUNT_CFG_FILE_NAME;
     if (!FileExists(configFile)) {
@@ -80,7 +80,7 @@ ErrCode OhosAccountDataDeal::Init(std::int32_t userId)
     return ERR_OK;
 }
 
-ErrCode OhosAccountDataDeal::AccountInfoFromJson(AccountInfo &accountInfo, const std::int32_t userId)
+ErrCode OhosAccountDataDeal::AccountInfoFromJson(AccountInfo &accountInfo, int32_t userId)
 {
     if (!initOk_) {
         ACCOUNT_LOGE("not init yet!");
@@ -98,7 +98,7 @@ ErrCode OhosAccountDataDeal::AccountInfoToJson(const AccountInfo &accountInfo) c
     return SaveAccountInfo(accountInfo);
 }
 
-int32_t OhosAccountDataDeal::SaveAccountInfo(const AccountInfo &accountInfo) const
+ErrCode OhosAccountDataDeal::SaveAccountInfo(const AccountInfo &accountInfo) const
 {
     std::string scalableDataStr = (accountInfo.ohosAccountInfo_.scalableData_).ToString();
     nlohmann::json jsonData = json {
@@ -130,7 +130,25 @@ int32_t OhosAccountDataDeal::SaveAccountInfo(const AccountInfo &accountInfo) con
     return ERR_OK;
 }
 
-int32_t OhosAccountDataDeal::GetAccountInfo(AccountInfo &accountInfo, const std::int32_t userId)
+ErrCode OhosAccountDataDeal::ParseJsonFromFile(const std::string &filePath, nlohmann::json &jsonData, int32_t userId)
+{
+    std::ifstream fin(filePath);
+    if (!fin) {
+        ACCOUNT_LOGE("Failed to open config file %{public}s, errno %{public}d.", filePath.c_str(), errno);
+        ReportOhosAccountOperationFail(userId, OPERATION_OPEN_FILE_TO_READ, errno, filePath);
+        return ERR_ACCOUNT_DATADEAL_INPUT_FILE_ERROR;
+    }
+    // NOT-allow exceptions when parse json file
+    jsonData = json::parse(fin, nullptr, false);
+    fin.close();
+    if (!jsonData.is_structured()) {
+        ACCOUNT_LOGE("Invalid json file,  %{public}s, remove", filePath.c_str());
+        return ERR_ACCOUNT_DATADEAL_JSON_FILE_CORRUPTION;
+    }
+    return ERR_OK;
+}
+
+ErrCode OhosAccountDataDeal::GetAccountInfo(AccountInfo &accountInfo, const int32_t userId)
 {
     std::string configFile = configFileDir_ + std::to_string(userId) + ACCOUNT_CFG_FILE_NAME;
     if (!FileExists(configFile)) {
@@ -140,19 +158,10 @@ int32_t OhosAccountDataDeal::GetAccountInfo(AccountInfo &accountInfo, const std:
         Restorecon(configFile.c_str());
 #endif // WITH_SELINUX
     }
-    std::ifstream fin(configFile);
-    if (!fin) {
-        ACCOUNT_LOGE("Failed to open config file %{public}s, errno %{public}d.", configFile.c_str(), errno);
-        ReportOhosAccountOperationFail(userId, OPERATION_OPEN_FILE_TO_READ, errno, configFile);
-        return ERR_ACCOUNT_DATADEAL_INPUT_FILE_ERROR;
-    }
-    // NOT-allow exceptions when parse json file
     std::lock_guard<std::mutex> lock(mutex_);
-    jsonData_ = json::parse(fin, nullptr, false);
-    fin.close();
-    if (!jsonData_.is_structured()) {
-        ACCOUNT_LOGE("Invalid json file,  %{public}s, remove", configFile.c_str());
-        return ERR_ACCOUNT_DATADEAL_JSON_FILE_CORRUPTION;
+    ErrCode ret = ParseJsonFromFile(configFile, jsonData_, userId);
+    if (ret != ERR_OK) {
+        return ret;
     }
 
     const auto &jsonObjectEnd = jsonData_.end();
@@ -169,7 +178,7 @@ int32_t OhosAccountDataDeal::GetAccountInfo(AccountInfo &accountInfo, const std:
     }
 
     if (jsonData_.find(DATADEAL_JSON_KEY_OHOSACCOUNT_STATUS) != jsonObjectEnd) {
-        accountInfo.ohosAccountInfo_.status_ = jsonData_.at(DATADEAL_JSON_KEY_OHOSACCOUNT_STATUS).get<std::int32_t>();
+        accountInfo.ohosAccountInfo_.status_ = jsonData_.at(DATADEAL_JSON_KEY_OHOSACCOUNT_STATUS).get<int32_t>();
     }
 
     if (jsonData_.find(DATADEAL_JSON_KEY_OHOSACCOUNT_NICKNAME) != jsonObjectEnd) {
@@ -193,7 +202,7 @@ int32_t OhosAccountDataDeal::GetAccountInfo(AccountInfo &accountInfo, const std:
     return ERR_OK;
 }
 
-void OhosAccountDataDeal::BuildJsonFileFromScratch(std::int32_t userId) const
+void OhosAccountDataDeal::BuildJsonFileFromScratch(int32_t userId) const
 {
     AccountInfo accountInfo;
     accountInfo.userId_ = userId;
