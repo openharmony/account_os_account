f3ca8d92bc9b975aa0de18f022d0ced32c0c81b0
lichenchen <lichenchen15@huawei.com>
2022-10-27 21:10:32 +0800

    账号模块watchDog功能适配
    Signed-off-by: lichenchen <lichenchen15@huawei.com>

--- services/accountmgr/include/account_mgr_service.h
+++ services/accountmgr/include/account_mgr_service.h
@@ -27,8 +27,6 @@
 #endif
 #include "account_stub.h"
 #include "app_account_manager_service.h"
-#include "event_handler.h"
-#include "event_runner.h"
 #include "os_account_manager_service.h"
 #include "iaccount.h"
 #include "iremote_object.h"
@@ -84,8 +82,6 @@ private:
     sptr<AppAccountManagerService> appAccountManagerService_ = nullptr;
     sptr<OsAccountManagerService> osAccountManagerService_ = nullptr;
     sptr<AccountIAMService> accountIAMService_ = nullptr;
-    std::shared_ptr<AppExecFwk::EventRunner> runner_ = nullptr;
-    std::shared_ptr<AppExecFwk::EventHandler> handler_ = nullptr;
 };
 }  // namespace AccountSA
 }  // namespace OHOS
