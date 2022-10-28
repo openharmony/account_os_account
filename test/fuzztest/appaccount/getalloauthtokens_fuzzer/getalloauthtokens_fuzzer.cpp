f5629bbd703ee39649db17c0d3d8d0f32a027e15
liushuling <liushuling1@huawei.com>
2022-10-28 10:32:16 +0800

    fixed 9391927 from https://gitee.com/strong-tree/account_os_account/pulls/622 补充FUZZ用例
    Signed-off-by: liushuling <liushuling1@huawei.com>

---
+++ test/fuzztest/appaccount/getalloauthtokens_fuzzer/getalloauthtokens_fuzzer.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "getalloauthtokens_fuzzer.h"
+
+#include "account_log_wrapper.h"
+#include "app_account_manager.h"
+#include "app_account_info.h"
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace OHOS::AccountSA;
+
+namespace OHOS {
+    bool GetAllOAuthTokensFuzzTest(const uint8_t* data, size_t size)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(reinterpret_cast<const char*>(data), size);
+            std::string testOwner(reinterpret_cast<const char*>(data), size);
+            std::string testAuthType(reinterpret_cast<const char*>(data), size);
+            std::vector<OAuthTokenInfo> testTokenInfos;
+            OAuthTokenInfo testTokenInfo;
+            testTokenInfo.authType = testAuthType;
+            testTokenInfos.emplace_back(testTokenInfo);
+            result = AppAccountManager::GetAllOAuthTokens(testName, testOwner, testTokenInfos);
+        }
+        return result;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::GetAllOAuthTokensFuzzTest(data, size);
+    return 0;
+}
+
