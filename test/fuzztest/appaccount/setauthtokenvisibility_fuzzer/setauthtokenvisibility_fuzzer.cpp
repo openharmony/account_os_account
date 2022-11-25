299ba900bdd1e0ebcf9962922d0bc83fb8d02947
lichenchen <lichenchen15@huawei.com>
2022-11-25 11:12:37 +0800

    账号新增接口Fuzz用例覆盖
    Signed-off-by: lichenchen <lichenchen15@huawei.com>

---
+++ test/fuzztest/appaccount/setauthtokenvisibility_fuzzer/setauthtokenvisibility_fuzzer.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "setauthtokenvisibility_fuzzer.h"
+
+#include "app_account_manager.h"
+#include "account_log_wrapper.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace OHOS::AccountSA;
+
+const int CONSTANTS_NUMBER_TWO = 2;
+
+namespace OHOS {
+    bool SetAuthTokenVisibilityFuzzTest(const uint8_t* data, size_t size)
+    {
+        bool result = false;
+        if (size > 0) {
+            std::string testName(reinterpret_cast<const char*>(data), size);
+            std::string testAuthType(reinterpret_cast<const char*>(data), size);
+            std::string testBundleName(reinterpret_cast<const char*>(data), size);
+            bool isVisible = ((size % CONSTANTS_NUMBER_TWO) == 0);
+            result = AppAccountManager::SetAuthTokenVisibility(testName, testAuthType, testBundleName, isVisible);
+        }
+        return result;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::SetAuthTokenVisibilityFuzzTest(data, size);
+    return 0;
+}
+
