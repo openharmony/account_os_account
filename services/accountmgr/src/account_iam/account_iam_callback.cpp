c8cc75f15abd206189f3199d6d385a82c3b6126b
jidong <jidong4@huawei.com>
2022-12-06 15:23:12 +0800

    fix domain account problem
    Signed-off-by: jidong <jidong4@huawei.com>
    Change-Id: I096887580e6d52f1e96298bbdeec46dacf91e1d7

--- services/accountmgr/src/account_iam/account_iam_callback.cpp
+++ services/accountmgr/src/account_iam/account_iam_callback.cpp
@@ -239,8 +239,8 @@ void DelCredCallback::OnAcquireInfo(int32_t module, uint32_t acquireInfo, const 
 }
 
 GetCredInfoCallbackWrapper::GetCredInfoCallbackWrapper(
-    const int32_t &userId, const sptr<IGetCredInfoCallback> &callback)
-    : userId_(userId), innerCallback_(callback)
+    int32_t userId, int32_t authType, const sptr<IGetCredInfoCallback> &callback)
+    : userId_(userId), authType_(authType), innerCallback_(callback)
 {}
 
 void GetCredInfoCallbackWrapper::OnCredentialInfo(const std::vector<CredentialInfo> &infoList)
@@ -248,14 +248,16 @@ void GetCredInfoCallbackWrapper::OnCredentialInfo(const std::vector<CredentialIn
     if (innerCallback_ == nullptr) {
         return;
     }
-    bool isAvailable = InnerAccountIAMManager::GetInstance().CheckDomainAuthAvailable(userId_);
-    if (isAvailable) {
-        std::vector<CredentialInfo> newInfoList = infoList;
-        CredentialInfo info;
-        info.authType = static_cast<AuthType>(IAMAuthType::DOMAIN);
-        info.pinType = static_cast<PinSubType>(IAMAuthSubType::DOMAIN_MIXED);
-        newInfoList.emplace_back(info);
-        return innerCallback_->OnCredentialInfo(newInfoList);
+    if (authType_ == 0) {
+        bool isAvailable = InnerAccountIAMManager::GetInstance().CheckDomainAuthAvailable(userId_);
+        if (isAvailable) {
+            std::vector<CredentialInfo> newInfoList = infoList;
+            CredentialInfo info;
+            info.authType = static_cast<AuthType>(IAMAuthType::DOMAIN);
+            info.pinType = static_cast<PinSubType>(IAMAuthSubType::DOMAIN_MIXED);
+            newInfoList.emplace_back(info);
+            return innerCallback_->OnCredentialInfo(newInfoList);
+        }
     }
     return innerCallback_->OnCredentialInfo(infoList);
 }
