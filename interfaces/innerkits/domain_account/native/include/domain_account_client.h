4c0a02012f17bbcf09722cb0edfa019f3644a9f0
jidong <jidong4@huawei.com>
2023-04-03 11:24:33 +0800

    add iam domain ohos account inner api annotation
    Signed-off-by: jidong <jidong4@huawei.com>

--- interfaces/innerkits/domain_account/native/include/domain_account_client.h
+++ interfaces/innerkits/domain_account/native/include/domain_account_client.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,6 +13,26 @@
  * limitations under the License.
  */
 
+/**
+ * @addtogroup DomainAccount
+ * @{
+ *
+ * @brief Provides domain account management.
+ *
+ * Provides the capability to manage domain accounts.
+ *
+ * @since 10.0
+ * @version 10.0
+ */
+
+/**
+ * @file domain_account_client.h
+ *
+ * @brief Declares domain account manager interfaces.
+ *
+ * @since 10.0
+ * @version 10.0
+ */
 #ifndef OS_ACCOUNT_INTERFACES_INNERKITS_DOMAIN_ACCOUNT_INCLUDE_DOMAIN_ACCOUNT_CLIENT_H
 #define OS_ACCOUNT_INTERFACES_INNERKITS_DOMAIN_ACCOUNT_INCLUDE_DOMAIN_ACCOUNT_CLIENT_H
 
@@ -27,14 +47,66 @@ namespace OHOS {
 namespace AccountSA {
 class DomainAccountClient {
 public:
+    /**
+     * Gets the instance of DomainAccountClient.
+     *
+     * @return the instance of DomainAccountClient.
+     */
     static DomainAccountClient &GetInstance();
+
+    /**
+     * @brief Registers the domain plugin, which provides the capabilities for domain authentication.
+     * @permission ohos.permission.MANAGE_LOCAL_ACCOUNTS
+     * @param plugin - Indicates the domain plugin.
+     * @return error code, see account_error_no.h
+     */
     ErrCode RegisterPlugin(const std::shared_ptr<DomainAccountPlugin> &plugin);
+
+    /**
+     * @brief Unregisters domain plugin.
+     * @permission ohos.permission.MANAGE_LOCAL_ACCOUNTS
+     * @return error code, see account_error_no.h
+     */
     ErrCode UnregisterPlugin();
+
+    /**
+     * @brief Authenticates the specified domain account with a credential.
+     * @permission ohos.permission.ACCESS_USER_AUTH_INTERNAL
+     * @param domainAccountInfo - Indicates the domain account information.
+     * @param password - Indicates the credential for authentication.
+     * @param callback - Indicates the callback for getting the authentication result.
+     * @return error code, see account_error_no.h
+     */
     ErrCode Auth(const DomainAccountInfo &info, const std::vector<uint8_t> &password,
         const std::shared_ptr<DomainAuthCallback> &callback);
+
+    /**
+     * @brief Authenticates a domain account bound with the specified userId with a credential.
+     * @permission ohos.permission.ACCESS_USER_AUTH_INTERNAL
+     * @param domainAccountInfo - Indicates the domain account information.
+     * @param password - Indicates the credential for authentication.
+     * @param callback - Indicates the callback for getting the authentication result.
+     * @return error code, see account_error_no.h
+     */
     ErrCode AuthUser(int32_t userId, const std::vector<uint8_t> &password,
         const std::shared_ptr<DomainAuthCallback> &callback);
+
+    /**
+     * @brief Authenticates the domain account bound to the specified OS account with a popup.
+     * @permission ohos.permission.ACCESS_USER_AUTH_INTERNAL
+     * @param localId - Indicates the local ID of the specified OS account.
+     * @param callback - Indicates the callback for getting the authentication result.
+     * @return error code, see account_error_no.h
+     */
     ErrCode AuthWithPopup(int32_t userId, const std::shared_ptr<DomainAuthCallback> &callback);
+
+    /**
+     * @brief Checks whether the specified domain account exists.
+     * @permission ohos.permission.MANAGE_LOCAL_ACCOUNTS
+     * @param domainAccountInfo - Indicates the domain account information.
+     * @param callback - Indicates the callback for checking whether the specified domain account exists.
+     * @return error code, see account_error_no.h
+     */
     ErrCode HasDomainAccount(const DomainAccountInfo &info, const std::shared_ptr<DomainAccountCallback> &callback);
 
 private:
