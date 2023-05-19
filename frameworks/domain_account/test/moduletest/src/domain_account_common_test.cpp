96be393d966c008ab3f1e651278210d9bc8f6149
123 <834689456@qq.com>
2023-05-19 16:31:42 +0800

    域账号用例补充
    Signed-off-by: 123 <834689456@qq.com>
    Change-Id: I810753543b82c28022de802d3fa50dd77b894916

---
+++ frameworks/domain_account/test/moduletest/src/domain_account_common_test.cpp
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cerrno>
+#include <gtest/gtest.h>
+#include "account_log_wrapper.h"
+#define private public
+#include "domain_account_common.h"
+#undef private
+#include "domain_account_client.h"
+#include "parcel.h"
+#include "want.h"
+using namespace testing;
+using namespace testing::ext;
+using namespace OHOS;
+using namespace OHOS::AccountSA;
+
+namespace {
+const int32_t CALLING_UID = 100;
+const int32_t REMAINING_TIMES = 100;
+const int32_t PARAM_ZERO = 0;
+const std::string STRING_NAME_TWO = "zhangsan666";
+const std::string STRING_DOMAIN_NEW = "test.example.com";
+const std::string STRING_NAME_NEW = "zhangsan777";
+const std::string STRING_ACCOUNTID_NEW = "3333";
+const std::vector<uint8_t> TOKEN = {1, 2, 3, 4, 5};
+} // namespace
+
+class DomainAccountCommonModuleTest : public testing::Test {
+public:
+    static void SetUpTestCase(void);
+    static void TearDownTestCase(void);
+    void SetUp();
+    void TearDown();
+};
+
+void DomainAccountCommonModuleTest::SetUpTestCase(void)
+{
+    GTEST_LOG_(INFO) << "SetUpTestCase enter";
+}
+
+void DomainAccountCommonModuleTest::TearDownTestCase(void)
+{
+    GTEST_LOG_(INFO) << "TearDownTestCase";
+}
+
+void DomainAccountCommonModuleTest::SetUp(void)
+{}
+
+void DomainAccountCommonModuleTest::TearDown(void)
+{}
+
+/**
+ * @tc.name: DomainAccountCommonModuleTest_GetAccessTokenOptions_001
+ * @tc.desc: GetAccessTokenOptions Marshalling successfully.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(DomainAccountCommonModuleTest, DomainAccountCommonModuleTest_GetAccessTokenOptions_001, TestSize.Level0)
+{
+    AAFwk::WantParams parameters;
+    GetAccessTokenOptions option(CALLING_UID, parameters);
+    Parcel parcel;
+    option.Marshalling(parcel);
+    GetAccessTokenOptions *getAccessTokenOptions = option.Unmarshalling(parcel);
+    std::shared_ptr<GetAccessTokenOptions> optionPtr(getAccessTokenOptions);
+    EXPECT_EQ(getAccessTokenOptions->callingUid_, CALLING_UID);
+}
+
+/**
+ * @tc.name: DomainAccountCommonModuleTest_DomainAuthResult_001
+ * @tc.desc: DomainAuthResult Marshalling successfully.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(DomainAccountCommonModuleTest, DomainAccountCommonModuleTest_DomainAuthResult_001, TestSize.Level0)
+{
+    DomainAuthResult domainAuthResult;
+    domainAuthResult.token = TOKEN;
+    Parcel parcel;
+    domainAuthResult.Marshalling(parcel);
+    DomainAuthResult *result = domainAuthResult.Unmarshalling(parcel);
+    std::shared_ptr<DomainAuthResult> domainAuthResultPtr(result);
+    for (size_t index = 0; index < result->token.size(); index++) {
+        EXPECT_EQ(result->token[index], TOKEN[index]);
+    }
+}
+
+/**
+ * @tc.name: DomainAccountCommonModuleTest_AuthStatusInfo_001
+ * @tc.desc: AuthStatusInfo Marshalling successfully.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(DomainAccountCommonModuleTest, DomainAccountCommonModuleTest_AuthStatusInfo_001, TestSize.Level0)
+{
+    AuthStatusInfo authStatusInfo;
+    authStatusInfo.remainingTimes = REMAINING_TIMES;
+    Parcel parcel;
+    authStatusInfo.Marshalling(parcel);
+    AuthStatusInfo *result = authStatusInfo.Unmarshalling(parcel);
+    std::shared_ptr<AuthStatusInfo> authStatusInfoPtr(result);
+    EXPECT_EQ(authStatusInfoPtr->remainingTimes, REMAINING_TIMES);
+}
+
+/**
+ * @tc.name: DomainAccountCommonModuleTest_DomainAccountListenerRecord_001
+ * @tc.desc: DomainAccountListenerRecord successfully.
+ * @tc.type: FUNC
+ * @tc.require:
+ */
+HWTEST_F(DomainAccountCommonModuleTest, DomainAccountCommonModuleTest_DomainAccountListenerRecord_001, TestSize.Level0)
+{
+    DomainAccountInfo domainInfo(STRING_DOMAIN_NEW, STRING_NAME_NEW, STRING_ACCOUNTID_NEW);
+    DomainAccountClient::DomainAccountListenerRecord record(domainInfo, nullptr);
+    EXPECT_EQ(record.infos_[PARAM_ZERO].accountName_, STRING_NAME_NEW);
+    DomainAccountClient::DomainAccountListenerRecord record1(nullptr);
+    EXPECT_EQ(record1.callback_, nullptr);
+}
