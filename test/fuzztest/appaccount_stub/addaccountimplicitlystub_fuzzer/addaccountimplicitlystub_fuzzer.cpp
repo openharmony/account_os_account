758ed81d56216db254b9b806655810c57a60dc8f
wuliushuan <wuliushuan.30044128@huawei.com>
2023-05-22 12:14:11 +0000

    add appaccount service FUZZ test
    Signed-off-by: wuliushuan <wuliushuan.30044128@huawei.com>
    Change-Id: I8e81fb44f0fa683b0ce83071391c5e0966485fe0

---
+++ test/fuzztest/appaccount_stub/addaccountimplicitlystub_fuzzer/addaccountimplicitlystub_fuzzer.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "addaccountimplicitlystub_fuzzer.h"
+
+#include "account_log_wrapper.h"
+#include "app_account_manager_service.h"
+#include "iapp_account.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace OHOS::AccountSA;
+
+const std::u16string APPACCOUNT_TOKEN = u"ohos.accountfwk.IAppAccount";
+namespace OHOS {
+    bool AddAccountImplicitlyStubFuzzTest(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return false;
+        }
+        MessageParcel dataTemp;
+        if (!dataTemp.WriteInterfaceToken(APPACCOUNT_TOKEN)) {
+            return false;
+        }
+        std::string owner(reinterpret_cast<const char*>(data), size);
+        if (!dataTemp.WriteString(owner)) {
+            return false;
+        }
+        std::string authType(reinterpret_cast<const char*>(data), size);
+        if (!dataTemp.WriteString(authType)) {
+            return false;
+        }
+        AAFwk::Want options;
+        if (!dataTemp.WriteParcelable(&options)) {
+            return false;
+        }
+        MessageParcel reply;
+        MessageOption option;
+        uint32_t code = static_cast<uint32_t>(IAppAccount::Message::ADD_ACCOUNT_IMPLICITLY);
+        auto appAccountManagerService = std::make_shared<AppAccountManagerService>();
+        appAccountManagerService->OnRemoteRequest(code, dataTemp, reply, option);
+        
+        return true;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::AddAccountImplicitlyStubFuzzTest(data, size);
+    return 0;
+}
+
