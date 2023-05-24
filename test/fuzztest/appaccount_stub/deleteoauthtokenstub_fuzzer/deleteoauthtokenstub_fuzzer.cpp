6745a86fc671c69adf23e591e54ca9d706390ee0
wuliushuan <wuliushuan.30044128@huawei.com>
2023-05-24 06:35:08 +0000

    appaccount service fuzz test add 0524_01
    Signed-off-by: wuliushuan <wuliushuan.30044128@huawei.com>
    Change-Id: I3dcbdd5d9380fa496d3a62c21d4a37b215f9b1af

---
+++ test/fuzztest/appaccount_stub/deleteoauthtokenstub_fuzzer/deleteoauthtokenstub_fuzzer.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "deleteoauthtokenstub_fuzzer.h"
+#include <string>
+#include <vector>
+#include "account_log_wrapper.h"
+#include "app_account_manager_service.h"
+#include "iapp_account.h"
+
+using namespace std;
+using namespace OHOS::AccountSA;
+const std::u16string APPACCOUNT_TOKEN = u"ohos.accountfwk.IAppAccount";
+namespace OHOS {
+    bool DeleteOAuthTokenStubFuzzTest(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return false;
+        }
+        MessageParcel dataTemp;
+        if (!dataTemp.WriteInterfaceToken(APPACCOUNT_TOKEN)) {
+            return false;
+        }
+        std::string name(reinterpret_cast<const char*>(data), size);
+        if (!dataTemp.WriteString(name)) {
+            return false;
+        }
+        std::string owner(reinterpret_cast<const char*>(data), size);
+        if (!dataTemp.WriteString(owner)) {
+            return false;
+        }
+        std::string authType(reinterpret_cast<const char*>(data), size);
+        if (!dataTemp.WriteString(authType)) {
+            return false;
+        }
+        std::string token(reinterpret_cast<const char*>(data), size);
+        if (!dataTemp.WriteString(token)) {
+            return false;
+        }
+        MessageParcel reply;
+        MessageOption option;
+        uint32_t code = static_cast<uint32_t>(IAppAccount::Message::DELETE_OAUTH_TOKEN);
+        auto appAccountManagerService = std::make_shared<AppAccountManagerService>();
+        appAccountManagerService->OnRemoteRequest(code, dataTemp, reply, option);
+        
+        return true;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::DeleteOAuthTokenStubFuzzTest(data, size);
+    return 0;
+}
+
