dbd826df1743aa89d6797a717eec024f511235e4
wuliushuan <wuliushuan.30044128@huawei.com>
2023-05-27 08:53:04 +0000

    osaccount service fuzz add 0527
    Signed-off-by: wuliushuan <wuliushuan.30044128@huawei.com>
    Change-Id: I5f9f85d7076ba5d8c2fd05b6c6edd77b5e9bf522

---
+++ test/fuzztest/osaccount_stub/proccreateosaccountstub_fuzzer/proccreateosaccountstub_fuzzer.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "proccreateosaccountstub_fuzzer.h"
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "ios_account.h"
+#include "os_account_manager_service.h"
+
+using namespace std;
+using namespace OHOS::AccountSA;
+
+namespace OHOS {
+const std::u16string IOS_ACCOUNT_DESCRIPTOR = u"ohos.accountfwk.IOsAccount";
+bool ProcCreateOsAccountStubFuzzTest(const uint8_t *data, size_t size)
+{
+    if ((data == nullptr) || (size <= 0)) {
+        return false;
+    }
+
+    std::string testName(reinterpret_cast<const char *>(data), size);
+
+    MessageParcel datas;
+    datas.WriteInterfaceToken(IOS_ACCOUNT_DESCRIPTOR);
+
+    if (!datas.WriteString(testName)) {
+        return false;
+    }
+    if (!datas.WriteInt32(OsAccountType::GUEST)) {
+        return false;
+    }
+
+    uint32_t code = static_cast<uint32_t>(IOsAccount::Message::CREATE_OS_ACCOUNT);
+
+    MessageParcel reply;
+    MessageOption option;
+
+    auto osAccountManagerService_ = std::make_shared<OsAccountManagerService>();
+
+    osAccountManagerService_ ->OnRemoteRequest(
+        code, datas, reply, option);
+
+    return true;
+}
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::ProcCreateOsAccountStubFuzzTest(data, size);
+    return 0;
+}
