fd1969f622862292e6626253ed7071692e8e27c0
steven_q <qichanggui@huawei.com>
2023-05-28 18:00:40 +0800

    add stub fuzz
    Signed-off-by: steven_q <qichanggui@huawei.com>
    Change-Id: I67c4324bd038f38ee986ff81878a3781964c957c

---
+++ test/fuzztest/iamaccount_stub/getcredentialinfostub_fuzzer/getcredentialinfostub_fuzzer.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "getcredentialinfostub_fuzzer.h"
+#include <string>
+#include <vector>
+#include "account_log_wrapper.h"
+#include "account_iam_service.h"
+#include "iaccount_iam.h"
+
+using namespace std;
+using namespace OHOS::AccountSA;
+namespace OHOS {
+const std::u16string IAMACCOUNT_TOKEN = u"ohos.accountfwk.IAccountIAM";
+bool GetCredentialInfoStubFuzzTest(const uint8_t *data, size_t size)
+{
+    if ((data == nullptr) || (size == 0)) {
+        return false;
+    }
+
+    MessageParcel dataTemp;
+    if (!dataTemp.WriteInterfaceToken(IAMACCOUNT_TOKEN)) {
+        return false;
+    }
+
+    int32_t userId = static_cast<int32_t>(size);
+    if (!dataTemp.WriteInt32(userId)) {
+        return false;
+    }
+
+    MessageParcel reply;
+    MessageOption option;
+    uint32_t code = static_cast<uint32_t>(IAccountIAM::Message::GET_CREDENTIAL_INFO);
+    auto iamAccountManagerService = std::make_shared<AccountIAMService>();
+    iamAccountManagerService->OnRemoteRequest(code, dataTemp, reply, option);
+
+    return true;
+}
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::GetCredentialInfoStubFuzzTest(data, size);
+    return 0;
+}
