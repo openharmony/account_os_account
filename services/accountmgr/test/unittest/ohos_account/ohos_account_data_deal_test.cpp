266ad245db43bec57e39d9cea2a3f5e0dfd9a0e5
steven_q <qichanggui@huawei.com>
2024-02-07 16:26:05 +0800

    add test log
    Signed-off-by: steven_q <qichanggui@huawei.com>
    Change-Id: I143fcca951e7db913646af564da21b6393a4b77b

--- services/accountmgr/test/unittest/ohos_account/ohos_account_data_deal_test.cpp
+++ services/accountmgr/test/unittest/ohos_account/ohos_account_data_deal_test.cpp
@@ -52,7 +52,15 @@ void OhosAccountDataDealTest::SetUpTestCase() {}
 
 void OhosAccountDataDealTest::TearDownTestCase() {}
 
-void OhosAccountDataDealTest::SetUp() {}
+void OhosAccountDataDealTest::SetUp(void) __attribute__((no_sanitize("cfi")))
+{
+    testing::UnitTest *test = testing::UnitTest::GetInstance();
+    ASSERT_NE(test, nullptr);
+    const testing::TestInfo *testinfo = test->current_test_info();
+    ASSERT_NE(testinfo, nullptr);
+    string testCaseName = string(testinfo->name());
+    ACCOUNT_LOGI("[SetUp] %{public}s start", testCaseName.c_str());
+}
 
 void OhosAccountDataDealTest::TearDown()
 {
