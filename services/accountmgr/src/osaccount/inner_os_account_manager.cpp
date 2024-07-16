456c48bd9e516ea90af1f7e2efb576e19421d5fb
cc_ggboy <chenweimin8@huawei.com>
2024-07-16 02:52:02 +0000

    代码整改与删除无用代码
    Signed-off-by: cc_ggboy <chenweimin8@huawei.com>

--- services/accountmgr/src/osaccount/inner_os_account_manager.cpp
+++ services/accountmgr/src/osaccount/inner_os_account_manager.cpp
@@ -177,6 +177,7 @@ void IInnerOsAccountManager::StartAccount()
     }
     // activate
     SendMsgForAccountActivate(osAccountInfo);
+    SetParameter(ACCOUNT_READY_EVENT.c_str(), "true");
 }
 
 void IInnerOsAccountManager::RestartActiveAccount()
@@ -1627,7 +1628,6 @@ ErrCode IInnerOsAccountManager::SendMsgForAccountActivate(OsAccountInfo &osAccou
     }
 
     PushIdIntoActiveList(localId);
-    SetParameter(ACCOUNT_READY_EVENT.c_str(), "true");
     OsAccountInterface::SendToCESAccountSwitched(localId, oldId);
     subscribeManager_.Publish(localId, OS_ACCOUNT_SUBSCRIBE_TYPE::ACTIVED);
     subscribeManager_.Publish(localId, oldId, OS_ACCOUNT_SUBSCRIBE_TYPE::SWITCHED);
