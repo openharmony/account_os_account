c4abfa4fd6d6d2b3f23e0b27cb77d6d03d81752d
liyimei <liyimei@huawei.com>
2024-10-28 16:11:31 +0800

    add code for support disallowed hap when upgrade system--merge release 5.0.1
    Signed-off-by: liyimei <liyimei@huawei.com>

--- services/accountmgr/src/osaccount/inner_os_account_manager.cpp
+++ services/accountmgr/src/osaccount/inner_os_account_manager.cpp
@@ -520,7 +520,8 @@ ErrCode IInnerOsAccountManager::ValidateShortName(const std::string &shortName)
     return ERR_OK;
 }
 
-ErrCode IInnerOsAccountManager::CreateOsAccountWithFullInfo(OsAccountInfo &osAccountInfo)
+ErrCode IInnerOsAccountManager::CreateOsAccountWithFullInfo(OsAccountInfo &osAccountInfo,
+    const CreateOsAccountOptions &options)
 {
     if (!pluginManager_.IsCreationAllowed()) {
         ACCOUNT_LOGI("Not allow creation account.");
@@ -538,7 +539,7 @@ ErrCode IInnerOsAccountManager::CreateOsAccountWithFullInfo(OsAccountInfo &osAcc
         RemoveLocalIdToOperating(osAccountInfo.GetLocalId());
         return errCode;
     }
-    errCode = SendMsgForAccountCreate(osAccountInfo);
+    errCode = SendMsgForAccountCreate(osAccountInfo, options);
     RemoveLocalIdToOperating(osAccountInfo.GetLocalId());
     if (errCode != ERR_OK) {
         (void)osAccountControl_->DelOsAccount(osAccountInfo.GetLocalId());
