4ab0911c8e2111b13e4ea6a772b6139dc7d2ac5f
cclicn <lichenchen22@huawei.com>
2024-11-04 21:55:44 +0800

    【账号iam】接口回调假异步流程优化整改
    Signed-off-by: cclicn <lichenchen22@huawei.com>

--- interfaces/kits/napi/account_iam/src/napi_account_iam_identity_manager.cpp
+++ interfaces/kits/napi/account_iam/src/napi_account_iam_identity_manager.cpp
@@ -470,13 +470,17 @@ napi_value NapiAccountIAMIdentityManager::GetAuthInfo(napi_env env, napi_callbac
             GetAuthInfoContext *context = reinterpret_cast<GetAuthInfoContext *>(data);
             auto idmCallback = std::make_shared<NapiGetInfoCallback>(
                 context->env, context->callbackRef, context->deferred);
+            if (idmCallback == nullptr) {
+                ACCOUNT_LOGE("Failed for nullptr");
+                return;
+            }
+            context->callbackRef = nullptr;
             if ((context->parseHasAccountId) && (!IsAccountIdValid(context->accountId))) {
                 std::vector<AccountSA::CredentialInfo> emptyInfoList;
                 idmCallback->OnCredentialInfo(ERR_JS_ACCOUNT_NOT_FOUND, emptyInfoList);
                 return;
             }
             AccountIAMClient::GetInstance().GetCredentialInfo(context->accountId, context->authType, idmCallback);
-            context->callbackRef = nullptr;
         },
         [](napi_env env, napi_status status, void *data) {
             delete reinterpret_cast<GetAuthInfoContext *>(data);
