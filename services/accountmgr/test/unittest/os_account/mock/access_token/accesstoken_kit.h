b31845f201b6e0eeaaffeeb4e179f96652205ca8
swg3156201044 <shiweigang2@huawei.com>
2025-02-27 16:47:42 +0800

    补充
    Signed-off-by: swg3156201044 <shiweigang2@huawei.com>

---
+++ services/accountmgr/test/unittest/os_account/mock/access_token/accesstoken_kit.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OS_ACCOUNT_SERVICES_ACCOUNTMGR_TEST_MOCK_APP_ACCOUNT_ACCESSTOKEN_KIT_H
+#define OS_ACCOUNT_SERVICES_ACCOUNTMGR_TEST_MOCK_APP_ACCOUNT_ACCESSTOKEN_KIT_H
+
+#include <string>
+#include "access_token.h"
+#include "hap_token_info.h"
+
+namespace OHOS {
+namespace Security {
+namespace AccessToken {
+class AccessTokenKit {
+public:
+    static ATokenTypeEnum GetTokenType(AccessTokenID tokenID);
+    static int VerifyAccessToken(unsigned int tokenID, const std::string &permissionName);
+    static int GetHapTokenInfo(AccessTokenID tokenID, HapTokenInfo& hapTokenInfoRes);
+};
+}  // namespace AccessToken
+}  // namespace Security
+}  // namespace OHOS
+
+#endif  // OS_ACCOUNT_SERVICES_ACCOUNTMGR_TEST_MOCK_APP_ACCOUNT_ACCESSTOKEN_KIT_H
