a87c93e03044ea58fe49928eacd5116c1626a892
yujann <yanjun59@huawei.com>
2025-05-21 15:24:43 +0800

    upload remove beforeall adjust
    Change-Id: If848c2bb264cb6d1768a1c1ea560fdf11ff61d8f
    Signed-off-by: yujann <yanjun59@huawei.com>

--- services/accountmgr/test/moduletest/os_account/inner_os_account_manager_test.cpp
+++ services/accountmgr/test/moduletest/os_account/inner_os_account_manager_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -42,7 +42,10 @@ void IInnerOsAccountManagerTest::SetUpTestCase(void)
 {}
 
 void IInnerOsAccountManagerTest::TearDownTestCase(void)
-{}
+{
+    std::string cmd = "chown -R 3058:3058 /data/service/el1/public/account";
+    system(cmd.c_str());
+}
 
 void IInnerOsAccountManagerTest::SetUp(void) __attribute__((no_sanitize("cfi")))
 {
--- services/accountmgr/test/unittest/os_account/os_account_event_manager_multiple_thread_test.cpp
+++ services/accountmgr/test/unittest/os_account/os_account_event_manager_multiple_thread_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -79,7 +79,10 @@ void OsAccountEventManagerTest::SetUpTestCase(void)
 }
 
 void OsAccountEventManagerTest::TearDownTestCase(void)
-{}
+{
+    std::string cmd = "chown -R 3058:3058 /data/service/el1/public/account";
+    system(cmd.c_str());
+}
 
 void OsAccountEventManagerTest::SetUp(void)
 {}
