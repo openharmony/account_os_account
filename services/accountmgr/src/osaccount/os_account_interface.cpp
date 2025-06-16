dd55ed5ca1e7aa06db597fcb0a203febc2d9a917
jidong <jidong4@huawei.com>
2025-06-16 09:21:57 +0800

    remove theme_mgr dependency
    Signed-off-by: jidong <jidong4@huawei.com>
    Change-Id: I5dba45bf16adec9788c9177b1f929d2ecaf33c0a

--- services/accountmgr/src/osaccount/os_account_interface.cpp
+++ services/accountmgr/src/osaccount/os_account_interface.cpp
@@ -229,23 +229,6 @@ ErrCode OsAccountInterface::SendToAMSAccountDeactivate(OsAccountInfo &osAccountI
     return code;
 }
 
-#ifdef HAS_THEME_SERVICE_PART
-void OsAccountInterface::InitThemeResource(int32_t localId)
-{
-    StartTraceAdapter("ThemeManager InitResource");
-    bool ret = ThemeManager::ThemeManagerClient::GetInstance().InitResource(localId);
-    if (!ret) {
-        ACCOUNT_LOGE("Init theme failed, localId=%{public}d.", localId);
-        REPORT_OS_ACCOUNT_FAIL(localId, Constants::OPERATION_CREATE, -1, "Failed to init theme resource");
-        FinishTraceAdapter();
-        return;
-    }
-    ACCOUNT_LOGI("Init theme successful.");
-    FinishTraceAdapter();
-    return;
-}
-#endif
-
 ErrCode OsAccountInterface::SendToBMSAccountCreate(
     OsAccountInfo &osAccountInfo, const std::vector<std::string> &disallowedHapList,
     const std::optional<std::vector<std::string>> &allowedHapList)
