b9afc18b0afc4e07f6e8d5b737e3a9c511851a42
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-07-16 18:30:40 +0800

    app_account 0702 commit
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

---
+++ frameworks/ets/taihe/app_account/src/taihe_appAccount_info.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "taihe_appAccount_info.h"
+
+namespace OHOS {
+namespace AccountSA {
+THauthenticatorAsyncCallback::THauthenticatorAsyncCallback()
+{}
+
+THauthenticatorAsyncCallback::~THauthenticatorAsyncCallback()
+{}
+
+ErrCode THauthenticatorAsyncCallback::OnResult(int32_t resultCode, const AAFwk::Want &result)
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (isDone_) {
+        return ERR_OK;
+    }
+    isDone_ = true;
+    param_ = std::make_shared<AuthenticatorCallbackParam>();
+    param_->resultCode = resultCode;
+    param_->result = result;
+    cv_.notify_one();
+    return ERR_OK;
+}
+
+ErrCode THauthenticatorAsyncCallback::OnRequestRedirected(const AAFwk::Want &request)
+{
+    return ERR_OK;
+};
+
+ErrCode THauthenticatorAsyncCallback::OnRequestContinued()
+{
+    return ERR_OK;
+};
+}
+}
