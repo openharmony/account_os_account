504b4b68b5176317f2fb8ca05cfa88ccde33e229
zhangzezhong <zhangzezhong8@huawei-partners.com>
2025-07-23 21:41:42 +0800

    taihe account 4 commit
    Signed-off-by: zhangzezhong <zhangzezhong8@huawei-partners.com>

---
+++ frameworks/ets/taihe/app_account/src/taihe_appAccount_info.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "taihe_appAccount_info.h"
+
+namespace OHOS {
+namespace AccountSA {
+SubscriberPtr::SubscriberPtr(const AccountSA::AppAccountSubscribeInfo &subscribeInfo,
+    taihe::callback_view<void(taihe::array_view<ohos::account::appAccount::AppAccountInfo>)>
+        callback):AccountSA::AppAccountSubscriber(subscribeInfo), callback_(callback)
+{}
+
+SubscriberPtr::~SubscriberPtr()
+{}
+
+void SubscriberPtr::OnAccountsChanged(const std::vector<AccountSA::AppAccountInfo> &accounts)
+{
+    std::lock_guard<std::mutex> lock(g_thLockForAppAccountSubscribers);
+    SubscriberPtr *subscriber = this;
+    bool isFound = false;
+    for (const auto& objectInfoTmp : AccountSA::g_ThAppAccountSubscribers) {
+        for (const auto& item : objectInfoTmp.second) {
+            if (item->subscriber.get() == subscriber) {
+                isFound = true;
+                break;
+            }
+        }
+        if (isFound) break;
+    }
+
+    if(!isFound) {
+        return;
+    }
+
+    std::vector<AccountSA::AppAccountInfo> tempAccountsInfos = accounts;
+    std::vector<ohos::account::appAccount::AppAccountInfo> tempInfo;
+    for (auto& accountInfo : tempAccountsInfos) {
+        ohos::account::appAccount::AppAccountInfo tempAccountInfo{
+            .owner = accountInfo.GetOwner().c_str(),
+            .name = accountInfo.GetName().c_str(),
+        };
+        tempInfo.push_back(tempAccountInfo);
+    }
+    active_callback call = callback_;
+    call(tempInfo);
+}
+}
+}
