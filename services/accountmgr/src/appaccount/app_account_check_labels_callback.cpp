c42a8eb8083ac700214243508877ff023388b806
yujann <yanjun59@huawei.com>
2025-11-13 15:45:54 +0800

    fix checkAccountLabels&selectAccountsByOptions in new api
    Change-Id: If9bdf151c025b710d41dafa49df0b0c0fa3ef103
    Signed-off-by: yujann <yanjun59@huawei.com>

--- services/accountmgr/src/appaccount/app_account_check_labels_callback.cpp
+++ services/accountmgr/src/appaccount/app_account_check_labels_callback.cpp
@@ -21,6 +21,12 @@
 
 namespace OHOS {
 namespace AccountSA {
+namespace {
+static const char* ACCOUNT_KEY = "account";
+static const char* ACCOUNT_OWNER_KEY = "owner";
+static const char* ACCOUNT_NAME_KEY = "name";
+}
+
 AppAccountCheckLabelsCallback::AppAccountCheckLabelsCallback(std::vector<AppAccountInfo> accounts,
     const AuthenticatorSessionRequest &request, const std::string &sessionId)
     : accounts_(accounts), request_(request), sessionId_(sessionId)
@@ -78,12 +84,54 @@ ErrCode AppAccountCheckLabelsCallback::OnResult(int32_t resultCode, const AAFwk:
         return ERR_OK;
     }
     isRequesting_ = false;
-    if (result.GetBoolParam(Constants::KEY_BOOLEAN_RESULT, false) && (index_ < accounts_.size())) {
-        accountsWithLabels_.push_back(accounts_[index_]);
+    if (result.HasParameter(Constants::KEY_BOOLEAN_RESULT)) {
+        // in old callback (below API 9), would ignore resultCode
+        OnResultAPI8(result);
+    } else {
+        // if KEY_BOOLEAN_RESULT not in result, it's new callback (API 9 and above) or
+        // considered as new callback
+        if (!OnResultAPI9(resultCode, result)) {
+            ACCOUNT_LOGE("Authenticator failed, resultCode = %{public}d", resultCode);
+            SendResult(ERR_JS_ACCOUNT_AUTHENTICATOR_SERVICE_EXCEPTION);
+            AppAccountAuthenticatorSessionManager::GetInstance().CloseSession(sessionId_);
+            return ERR_OK;
+        }
     }
     index_++;
     CheckLabels();
     return ERR_OK;
+}
+
+void AppAccountCheckLabelsCallback::OnResultAPI8(const AAFwk::Want &result)
+{
+    if (result.GetBoolParam(Constants::KEY_BOOLEAN_RESULT, false) && (index_ < accounts_.size())) {
+        accountsWithLabels_.push_back(accounts_[index_]);
+    }
+}
+
+bool AppAccountCheckLabelsCallback::OnResultAPI9(int32_t resultCode, const AAFwk::Want &result)
+{
+    if ((resultCode == ERR_JS_ACCOUNT_NOT_FOUND) || (index_ >= accounts_.size())) {
+        // Authenticator return account not found, consider not found.
+        return true;
+    }
+    if (resultCode != ERR_OK) {
+        // if resultCode is not ERR_JS_ACCOUNT_NOT_FOUND, considered as ERR_JS_ACCOUNT_AUTHENTICATOR_SERVICE_EXCEPTION
+        return false;
+    }
+    NativeAppAuthResult authResult(result);
+    if (!authResult.hasAuthResult || !authResult.account.has_value()) {
+        // Do not have auth result, consider check failed
+        ACCOUNT_LOGW("Do not have auth result or account info.");
+        return true;
+    }
+    if ((authResult.account->name_ == accounts_[index_].name_) &&
+        (authResult.account->owner_ == accounts_[index_].owner_)) {
+        accountsWithLabels_.push_back(accounts_[index_]);
+        return true;
+    }
+    ACCOUNT_LOGE("Check labels failed, name or owner not match");
+    return true;
 }
 
 ErrCode AppAccountCheckLabelsCallback::OnRequestRedirected(const AAFwk::Want &request)
@@ -110,6 +158,84 @@ ErrCode AppAccountCheckLabelsCallback::OnRequestContinued()
     index_++;
     CheckLabels();
     return ERR_OK;
+}
+
+NativeAppAuthResult::NativeAppAuthResult(const AAFwk::Want &result)
+{
+    auto params = result.GetParams();
+    if (params.IsEmpty()) {
+        ACCOUNT_LOGE("Param in want is empty");
+        return;
+    }
+    hasAuthResult = true;
+    auto accountWant = params.GetWantParams(ACCOUNT_KEY);
+    if (!accountWant.IsEmpty()) {
+        auto accountName = accountWant.GetStringParam(ACCOUNT_NAME_KEY);
+        auto accountOwner = accountWant.GetStringParam(ACCOUNT_OWNER_KEY);
+        AppAccountInfo accountInfo(accountName, accountOwner);
+        account = accountInfo;
+    }
+}
+
+CheckLabelsCallbackHelper::CheckLabelsCallbackHelper(
+    const std::string &name, const std::string &owner, const sptr<IAppAccountAuthenticatorCallback> &callback)
+    : name_(name), owner_(owner), callback_(callback)
+{}
+
+ErrCode CheckLabelsCallbackHelper::OnRequestRedirected(const AAFwk::Want &request)
+{
+    std::lock_guard<std::recursive_mutex> lock(mutex_);
+    return callback_->OnRequestRedirected(request);
+}
+
+ErrCode CheckLabelsCallbackHelper::OnRequestContinued()
+{
+    std::lock_guard<std::recursive_mutex> lock(mutex_);
+    return callback_->OnRequestContinued();
+}
+
+ErrCode CheckLabelsCallbackHelper::OnResult(int32_t resultCode, const AAFwk::Want &result)
+{
+    std::lock_guard<std::recursive_mutex> lock(mutex_);
+    if (result.HasParameter(Constants::KEY_BOOLEAN_RESULT)) {
+        // in old callback (below API 9), would do nothing
+        return callback_->OnResult(resultCode, result);
+    } else {
+        // if KEY_BOOLEAN_RESULT not in result, it's new callback (API 9 and above) or
+        // considered as new callback
+        return OnResultAPI9(resultCode, result);
+    }
+}
+
+ErrCode CheckLabelsCallbackHelper::OnResultAPI9(int32_t resultCode, const AAFwk::Want &result)
+{
+    AAFwk::Want want;
+    AAFwk::WantParams params;
+    if (resultCode == ERR_JS_ACCOUNT_NOT_FOUND) {
+        // Authenticator return account not found, should return ERR_JS_ACCOUNT_NOT_FOUND
+        return callback_->OnResult(ERR_JS_ACCOUNT_NOT_FOUND, want);
+    }
+    if (resultCode != ERR_OK) {
+        // if resultCode is other code, considered as ERR_JS_ACCOUNT_AUTHENTICATOR_SERVICE_EXCEPTION
+        ACCOUNT_LOGE(
+            "Authenticator failed during check labels, considered as authenticator error, resultCode = %{public}d",
+            resultCode);
+        return callback_->OnResult(ERR_JS_ACCOUNT_AUTHENTICATOR_SERVICE_EXCEPTION, want);
+    }
+    NativeAppAuthResult authResult(result);
+    if (!authResult.hasAuthResult || !authResult.account.has_value()) {
+        ACCOUNT_LOGE("Auth result or account is not found");
+        // Do not have auth result, consider check failed
+        want.SetParam(Constants::KEY_BOOLEAN_RESULT, false);
+        return callback_->OnResult(resultCode, want);
+    }
+    if ((authResult.account->name_ == name_) && (authResult.account->owner_ == owner_)) {
+        want.SetParam(Constants::KEY_BOOLEAN_RESULT, true);
+        return callback_->OnResult(resultCode, want);
+    }
+    ACCOUNT_LOGE("Check labels failed, name or owner not match");
+    want.SetParam(Constants::KEY_BOOLEAN_RESULT, false);
+    return callback_->OnResult(resultCode, want);
 }
 }  // namespace AccountSA
 }  // namespace OHOS
