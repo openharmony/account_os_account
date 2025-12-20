a9336c473ef343def7c716b729d4511ae4c9c9c1
LeonChan525 <chenwei26@huawei.com>
2025-12-20 18:21:41 +0800

    add fuzz for os account
    Signed-off-by: LeonChan525 <chenwei26@huawei.com>

---
+++ test/fuzztest/osaccount_stub/createosaccountwithfullinfoinosaccountinfo_fuzzer/createosaccountwithfullinfoinosaccountinfo_fuzzer.cpp
@@ -0,0 +1,148 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "createosaccountwithfullinfoinosaccountinfo_fuzzer.h"
+#include "os_account_stub.h"
+#include "account_log_wrapper.h"
+#include "fuzz_data.h"
+#include "os_account_manager_service.h"
+#include <string>
+#include <vector>
+
+using namespace OHOS::AccountSA;
+
+const int32_t TEST_USER_ID = 1006;
+const int32_t OS_ACCOUNT_TYPE_NUM = 5;
+const int64_t TEST_TIME_STAMP = 1695883215000;
+const std::u16string IOS_ACCOUNT_DESCRIPTOR = u"ohos.accountfwk.IOsAccount";
+const std::string TEST_HAP_STRING = "test_hap1";
+
+namespace OHOS {
+    bool CreateOsAccountWithFullInfoInOsAccountInfoStubFuzzTest(const uint8_t* data, size_t size)
+    {
+        if ((data == nullptr) || (size == 0)) {
+            return false;
+        }
+        
+        FuzzData fuzzData(data, size);
+        MessageParcel dataParcel;
+        if (!dataParcel.WriteInterfaceToken(IOS_ACCOUNT_DESCRIPTOR)) {
+            return false;
+        }
+        
+        auto useOsAccountInfo = fuzzData.GenerateBool();
+        if (useOsAccountInfo) {
+            // Create OsAccountInfo object for fuzzing
+            OsAccountInfo osAccountInfo;
+            osAccountInfo.SetLocalId(fuzzData.GetData<int32_t>());
+            osAccountInfo.SetLocalName(fuzzData.GenerateString());
+            osAccountInfo.SetShortName(fuzzData.GenerateString());
+            osAccountInfo.SetType(static_cast<OsAccountType>(fuzzData.GetData<int32_t>() % OS_ACCOUNT_TYPE_NUM));
+            osAccountInfo.SetCreateTime(TEST_TIME_STAMP);
+            osAccountInfo.SetLastLoginTime(TEST_TIME_STAMP);
+            
+            if (!dataParcel.WriteParcelable(&osAccountInfo)) {
+                return false;
+            }
+        }
+
+        MessageParcel reply;
+        MessageOption option;
+        auto osAccountManagerService_ = std::make_shared<OsAccountManagerService>();
+        osAccountManagerService_->OnRemoteRequest(
+            static_cast<uint32_t>(IOsAccountIpcCode::COMMAND_CREATE_OS_ACCOUNT_WITH_FULL_INFO_IN_OSACCOUNTINFO),
+            dataParcel, reply, option);
+        return true;
+    }
+
+    void SetProfilePhoto()
+    {
+        MessageParcel datas;
+        datas.WriteInterfaceToken(IOS_ACCOUNT_DESCRIPTOR);
+        datas.WriteInt32(TEST_USER_ID);
+        string photo = "test profile photo";
+        StringRawData stringRawData;
+        stringRawData.Marshalling(photo);
+        datas.WriteUint32(stringRawData.size);
+        datas.WriteRawData(stringRawData.data, stringRawData.size);
+        MessageParcel reply;
+        MessageOption option;
+        auto osAccountManagerService_ = std::make_shared<OsAccountManagerService>();
+
+        osAccountManagerService_->OnRemoteRequest(
+            static_cast<int32_t>(IOsAccountIpcCode::COMMAND_SET_OS_ACCOUNT_PROFILE_PHOTO), datas, reply, option);
+    }
+
+    void CleanOsAccount()
+    {
+        MessageParcel data;
+        MessageParcel reply;
+        MessageOption option;
+        data.WriteInterfaceToken(IOS_ACCOUNT_DESCRIPTOR);
+        data.WriteInt32(TEST_USER_ID);
+
+        auto osAccountManagerService_ = std::make_shared<OsAccountManagerService>();
+        osAccountManagerService_->OnRemoteRequest(
+            static_cast<uint32_t>(IOsAccountIpcCode::COMMAND_IS_OS_ACCOUNT_DEACTIVATING),
+            data, reply, option);
+        MessageParcel datas;
+        datas.WriteInterfaceToken(IOS_ACCOUNT_DESCRIPTOR);
+        datas.WriteInt32(TEST_USER_ID);
+        osAccountManagerService_->OnRemoteRequest(
+            static_cast<uint32_t>(IOsAccountIpcCode::COMMAND_REMOVE_OS_ACCOUNT),
+            datas, reply, option);
+    }
+
+    void GetCreatedOsAccountsCount()
+    {
+        MessageParcel datas;
+        datas.WriteInterfaceToken(IOS_ACCOUNT_DESCRIPTOR);
+        MessageOption option;
+        MessageParcel reply;
+        auto osAccountManagerService_ = std::make_shared<OsAccountManagerService>();
+        osAccountManagerService_->OnRemoteRequest(
+            static_cast<uint32_t>(IOsAccountIpcCode::COMMAND_GET_CREATED_OS_ACCOUNTS_COUNT),
+            datas, reply, option);
+    }
+
+    void QueryMaxOsAccountNumber()
+    {
+        MessageParcel datas;
+        datas.WriteInterfaceToken(IOS_ACCOUNT_DESCRIPTOR);
+        MessageOption option;
+        MessageParcel reply;
+        auto osAccountManagerService_ = std::make_shared<OsAccountManagerService>();
+        osAccountManagerService_->OnRemoteRequest(
+            static_cast<uint32_t>(IOsAccountIpcCode::COMMAND_QUERY_MAX_OS_ACCOUNT_NUMBER),
+            datas, reply, option);
+    }
+} // namespace OHOS
+
+extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv)
+{
+    OHOS::SetProfilePhoto();
+    OHOS::CleanOsAccount();
+    OHOS::GetCreatedOsAccountsCount();
+    OHOS::QueryMaxOsAccountNumber();
+    return 0;
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::CreateOsAccountWithFullInfoInOsAccountInfoStubFuzzTest(data, size);
+    return 0;
+}
